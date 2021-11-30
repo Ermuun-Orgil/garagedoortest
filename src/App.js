8eaab1f761d52be4c5be3fae7b2713786b003b31
IceCream <icecream@IceCreams-MacBook-Pro.local>
2021-11-30 11:01:09 +0800

    check readme pls

--- src/App.js
+++ src/App.js
@@ -3,23 +3,36 @@ import {Buffer} from 'buffer';
 import axios from "axios";
 
 const App = () => {
+
+  // post req yvuulah url http uchir CORSiin aldaa garaad bga tur zuur test hiihiin tuld heroku ashiglaad hiichihsen bga
+
   const url = "https://cors-anywhere.herokuapp.com/http://45.77.39.206:8081/api/v4/mqtt/publish";
   const username = 'admin';
   const password = 'public';
+  
+  //  haalga ongoilgoh json 
+
   const Opdata = {
     topic: "home/living",
     payload: { id: "E8:DB:84:AD:B2:34", command: "on" },
     qos: 0,
     retain: false,
+    //garagenii haalganii id odoogiin baidlaar static ugchihsun bga databaseesee avah ystoi
     clientid: "869523050100420",
   };
+  
+  //  haalga haah json 
+
   const Cldata = {
     topic: "home/living",
     payload: { id: "E8:DB:84:AD:B2:34", command: "off" },
     qos: 0,
     retain: false,
+    //garagenii haalganii id odoogiin baidlaar static ugchihsun bga databaseesee avah ystoi
     clientid: "869523050100420",
   };
+
+  // tsaadah serverluu axiosoor yvuulj bui reqiin function 
 
   const doorOpen = () => {
     axios
